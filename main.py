217789a0dc216fcda01bb56772a1b765077be0bd
leventkuru <32267806+leventkuru@users.noreply.github.com>
2021-01-20 07:00:40 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-from datetime import datetime
 import mysql.connector
-import timeago
-import hashlib
-from slugify import slugify
 from flask import Flask, url_for, render_template, redirect, request, session
 
 app = Flask(__name__)
 app.debug = True
+app.config['SECRET_KEY'] = '1234'
 
 mydb = mysql.connector.connect(
     host="localhost",
@@ -16,7 +13,6 @@ mydb = mysql.connector.connect(
 )
 
 mycursor = mydb.cursor(buffered=True, dictionary=True)
-
 
 
 def hasUser(email, table_name):
@@ -31,7 +27,7 @@ def hasUser(email, table_name):
         return False
 
 
-def update(type, id, name, surname, email, department):
+def updateData(type, id, name, surname, email, department):
     if type == 'students':
         typeIdName = "schoolId"
     elif type == 'instructors':
@@ -39,13 +35,83 @@ def update(type, id, name, surname, email, department):
     elif type == 'assistants':
         typeIdName = "assistantId"
     sql = "UPDATE " + type + " SET name = %s, surname = %s, email = %s, department = %s WHERE " + typeIdName + " = %s"
-    mycursor.execute(sql, (name, surname, email, department, updateId))
+    mycursor.execute(sql, (name, surname, email, department, id))
     mydb.commit()
 
 
-@app.route('/')
+def updateCourse(lessonName, location, startTime, endTime):
+    sql = "UPDATE lessons SET lessonName = %s, location = %s, startTime = %s, endTime = %s WHERE CRN = %s"
+    mycursor.execute(sql, (lessonName, location, startTime, endTime, updateId))
+    mydb.commit()
+
+
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('index.html')
+    sql = "SELECT *," \
+          " instructors.name AS iName," \
+          " students.name AS sName, " \
+          "assistants.name AS aName" \
+          " FROM lessons " \
+          "INNER JOIN students ON students.schoolId = lessons.schoolId " \
+          "INNER JOIN assistants ON assistants.assistantId = lessons.assistantId " \
+          "INNER JOIN instructors ON instructors.instructorId = lessons.instructorId " \
+          "ORDER BY CRN DESC"
+    mycursor.execute(sql)
+    lessons = mycursor.fetchall()
+    if request.method == 'POST':
+        if request.form['action'] == 'delete':
+            sql = "DELETE FROM lessons WHERE CRN = %s"
+            mycursor.execute(sql, (request.form['id'],))
+            mydb.commit()
+            return redirect(url_for('home'))
+        elif request.form['action'] == 'update':
+            global updateId
+            updateId = request.form['id']
+            session['updateId'] = updateId
+            return redirect(url_for('addCourse'))
+    return render_template('index.html', lessons=lessons)
+
+
+@app.route('/addCourse', methods=['GET', 'POST'])
+def addCourse():
+    error = ''
+    if request.method == 'POST':
+        if request.form['lessonName'] == '':
+            error = 'Lesson name cannot be empty'
+        elif request.form['location'] == '':
+            error = 'Location cannot be empty'
+        elif request.form['startTime'] == '':
+            error = 'Start time cannot be empty'
+        elif request.form['endTime'] == '':
+            error = 'End time cannot be empty'
+        elif request.form['radio'] == 'add':
+            if request.form['instructorId'] == '':
+                error = 'Instructor id cannot be empty'
+            elif request.form['schoolId'] == '':
+                error = 'School id cannot be empty'
+            elif request.form['assistantId'] == '':
+                error = 'Assistant id cannot be empty'
+            else:
+                sql = "INSERT INTO lessons SET lessonName = %s, location = %s, instructorId = %s," \
+                      " schoolId = %s, assistantId = %s, startTime = %s, endTime = %s"
+                mycursor.execute(sql, (
+                    request.form['lessonName'], request.form['location'], request.form['instructorId'],
+                    request.form['schoolId'],
+                    request.form['assistantId'], request.form['startTime'], request.form['endTime']))
+                mydb.commit()
+                return redirect(url_for('home'))
+        elif request.form['radio'] == 'update':
+            if request.form['instructorId'] != '':
+                error = 'Please leave instructor id empty'
+            elif request.form['schoolId'] != '':
+                error = 'Please leave school id empty'
+            elif request.form['assistantId'] != '':
+                error = 'Please leave assistant id empty'
+            else:
+                updateCourse(request.form['lessonName'], request.form['location'], request.form['startTime'],
+                             request.form['endTime'])
+                return redirect(url_for('home'))
+    return render_template('addCourse.html', error=error, update_id = session.get("updateId",None))
 
 
 @app.route('/data', methods=['GET', 'POST'])
@@ -61,7 +127,9 @@ def data():
         elif request.form['department'] == '':
             error = 'Department cannot be empty'
         elif request.form['radio'] == 'add':
-            if hasUser(request.form['email'], "students") or hasUser(request.form['email'], "instructors") or hasUser(
+            if request.form['updateId'] != '':
+                error = 'Update id should be empty for adding values'
+            elif hasUser(request.form['email'], "students") or hasUser(request.form['email'], "instructors") or hasUser(
                     request.form['email'], "assistants"):
                 error = 'This email has already been registered'
             else:
@@ -72,10 +140,10 @@ def data():
                 mydb.commit()
                 return redirect(url_for('home'))
         elif request.form['radio'] == 'update':
-            update(request.form['entType'], updateId, request.form['name'], request.form['surname'],
-                   request.form['email'], request.form['department'])
+            updateData(request.form['entType'], request.form['updateId'], request.form['name'], request.form['surname'],
+                       request.form['email'], request.form['department'])
             return redirect(url_for('home'))
-    return render_template('data.html', error=error)
+    return render_template('data.html', error=error, update_id = session.get("updateId",None))
 
 
 @app.route('/student', methods=['GET', 'POST'])
@@ -93,6 +161,7 @@ def student():
         elif request.form['action'] == 'update':
             global updateId
             updateId = request.form['id']
+            session['updateId'] = updateId
             return redirect(url_for('data'))
     return render_template('student.html', students=students)
 
